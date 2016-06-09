31aa0a1de82746241f60f907edd8417898c1e993
Cody Loyd <codyloyd@gmail.com>
2016-06-09 11:39:01 -0500

    added ceaser function, added style

--- app.rb
+++ app.rb
@@ -2,21 +2,29 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 
 
-get '/hi' do 
-	'hello wurld'
-end
-
 get '/' do 
 	erb :index
 end
 
 post '/' do
-	@message = translate(params[:text])
+	@message = cipher(params[:text],params[:key])
 	erb :index 
 end
 
+def shift_char(result, offset)
+  (((result - offset) % 26) + offset).chr
+end
 
-
-def translate(word)
-	word + "poop"
+def cipher(message,key)
+	array = message.scan(/./)		
+	array.map! do |char|
+	  if char.ord.between?(64,91)
+	    shift_char(char.ord + key.to_i, 65)
+	  elsif char.ord.between?(96,123)
+	    shift_char(char.ord + key.to_i, 97)
+	  else
+	    char
+	  end
+	end
+	array.join
 end
